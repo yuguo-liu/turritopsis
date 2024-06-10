a0f51f706f72d1c3b0078f136c92723fc7529cb5
fascy <fascy97>
2024-06-10 13:54:36 +0800

    test

--- run_local_network_test.sh
+++ run_local_network_test.sh
@@ -9,11 +9,10 @@ killall python3
 i=0
 while [ "$i" -lt $1 ]; do
     echo "start node $i..."
-    python3 run_socket_adkr.py --sid 'sidA' --id $i --N $1  --f $2 --K $3  --P "adkr-op"  --D True --pf 4 &
+    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --Ng $2 --f $3 --l $4 --B $5 --K $6 --recon 10 --P "adkr"  --O True &
     # python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --S 100 --P "ng" --D True --O True --C $4 &
     # python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --P "sdumbo" --O True &
 
     i=$(( i + 1 ))
 
 done
-
